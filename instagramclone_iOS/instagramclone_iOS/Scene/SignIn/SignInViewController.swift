993604811e49528a3fdbcfe559272515ac081077
전지훈 <junji636@gmail.com>
2021-06-01 17:03:26 +0900

    회원가입, 로그인화면 뷰생성

---
+++ instagramclone_iOS/instagramclone_iOS/Scene/SignIn/SignInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignInViewController.swift
+//  instagramclone_iOS
+//
+//  Created by 전지훈 on 2021/06/01.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ instagramclone_iOS/instagramclone_iOS/Scene/SignUp/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  instagramclone_iOS
+//
+//  Created by 전지훈 on 2021/06/01.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
